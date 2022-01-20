7fe93da6d42c2f2e25f578793f069ae3267fc13a
zaid.nezam_infobeans <zaid.nezam_infobeans@IB-SEZ-LAP-984>
2022-01-20 13:18:13 +0530

    Changes in screenshot

--- EVO3_TAF/src/main/java/com/evo/commonUtils/Screenshot.java
+++ EVO3_TAF/src/main/java/com/evo/commonUtils/Screenshot.java
@@ -15,35 +15,18 @@ public class Screenshot {
 	
 	public static void capture(WebDriver driver,String screenshotName) {
 	 
-	try 
-	{
+	try {
 		TakesScreenshot ts = (TakesScreenshot)BrowserFactory.LaunchBrowser();
+	 
 		File source = ts.getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(source,new File("D:\\Florida Press Report\\ScreenShots\\"+screenshotName+".png"));
-		//System.out.println("Screenshot taken");
+	 
+		FileUtils.copyFile(source,new File("Screenshots\\"+screenshotName+".png"));
+	 
+		System.out.println("Screenshot taken");
 	} 
 	catch (Exception e)
 	{
 		System.out.println("Exception while taking screenshot "+e.getMessage());
 	} 
-	}
-	
-	
-	public static void takeScreenShot(String methodname) throws IOException, InterruptedException { 
-		
-		Date currentdate = new Date();
-		String fname = currentdate.toString().replace(" ","_").replace(":","-");
-		Actions build = new Actions(BrowserFactory.LaunchBrowser());
-		
-		capture(BrowserFactory.LaunchBrowser(), (methodname+"_"+"1"+fname));
-		Thread.sleep(500);
-		
-		build.sendKeys(Keys.END).build().perform();
-		Thread.sleep(500);
-		capture(BrowserFactory.LaunchBrowser(), (methodname+"_"+"2"+fname));
-		Thread.sleep(200);
-		
-		build.sendKeys(Keys.HOME).build().perform();
-		Thread.sleep(500);
-	}
+	}	
 }
